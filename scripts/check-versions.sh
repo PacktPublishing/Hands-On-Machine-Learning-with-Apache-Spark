3e0021beb6a5be1aaa4b7ba485cd196b5e17683c
Tom Lous <tomlous@gmail.com>
2019-03-03 15:50:08 +0100

    Added Python check

--- scripts/check-versions.sh
+++ scripts/check-versions.sh
@@ -12,6 +12,10 @@ echo -n "Scala version ... "
 SCALA=$(scala -version 2>&1 | awk '{print $5}' | sed -n ';s/\(.*\)\.\(.*\)\..*/\1.\2/p;' | bc -l)
 if (( $(echo "$SCALA < 2.11" | bc -l) )); then echo "$SCALA is too old"; else echo "OK"; fi
 
+echo -n "Python version ... "
+PYTHON=$(python3 --version 2>&1| awk '{print $2}' |  sed -n ';s/\(.*\)\.\(.*\)\..*/\1.\2/p;' | bc -l)
+if (( $(echo "$PYTHON < 3.4" | bc -l) )); then echo "$PYTHON is too old"; else echo "OK"; fi
+
 echo -n "sbt version ... "
 SBT=$(sbt sbtVersion 2>&1 | grep "info" | cat -v | awk '{ print  substr($2,11,length($2)-15) }' | sed -n ';s/\(.*\)\.\(.*\)\..*/\1.\2/p;'| bc -l)
 if (( $(echo "$SBT < 1.0" | bc -l) )); then echo "$SBT is too old"; else echo "OK"; fi
